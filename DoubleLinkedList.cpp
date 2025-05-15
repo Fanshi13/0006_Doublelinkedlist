c38a5364f3cac89af023e6d0eea567e8a7c5196c
Fanshi13 <aquillafazanaufan@gmail.com>
2025-05-15 10:32:26 +0700

    commit 2

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node
+{
+public:
+    int nodeMhs;
+    node *next;
+    node *previous;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()(
+        START = NULL;)
+
+        void addNode()(
+            int nim;
+            string nim;
+            cout << "\nEnter the roll number of the student";
+            cin >> nim;
+
+            // Step 1: Allocate memory for new node
+            Node *newNode = new Node();)
+}
